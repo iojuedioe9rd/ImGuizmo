c560ca7cdbaa109205cec9cb915231a9520d9b0f
cats studio <79156677+iojuedioe9rd@users.noreply.github.com>
2024-10-19 15:25:47 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+    {
+    	"ImGuizmo.h",
+    	"ImGuizmo.cpp",
+    	"ImCurveEdit.h",
+    	"ImCurveEdit.cpp",
+    	"ImGradient.h",
+    	"ImGradient.cpp",
+    	"ImSequencer.h",
+    	"ImSequencer.cpp",
+	}
+
+    
+	filter "system:windows"
+    	systemversion "latest"
+
+	filter "configurations:Debug"
+    	runtime "Debug"
+    	symbols "on"
+
+	filter "configurations:Release"
+    	runtime "Release"
+    	symbols "on"
+    	optimize "on"
+    filter "configurations:Dist"
+        runtime "Release"
+        symbols "off"
+        optimize "full"
